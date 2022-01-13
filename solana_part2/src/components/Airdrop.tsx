46669eb7b5b4985546a3a25feab8b6ed24838462
Jorge Londoño <jmlon@yahoo.com>
2022-01-13 13:54:38 -0500

    Implemented updating the balance

--- solana_part2/src/components/Airdrop.tsx
+++ solana_part2/src/components/Airdrop.tsx
@@ -1,5 +1,5 @@
 import { Connection, PublicKey, clusterApiUrl, RpcResponseAndContext, SignatureResult } from "@solana/web3.js";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 interface AirdropProps {
     pubkey: PublicKey;
@@ -13,6 +13,14 @@ const Airdrop: FC<AirdropProps> = ({pubkey}) => {
     const [ lamports, setLamports ] = useState(100000);
     const [ txid, setTxid ] = useState<string | null>(null);
     const [ slot, setSlot ] = useState<number | null>(null);
+    const [ balance, setBalance ] = useState(0);
+
+    // Retrieve the balance when mounting the component
+    useEffect( () => {
+        const connection = new Connection(clusterApiUrl(network));
+        connection.getBalance(pubkey).then(setBalance);
+    }, []);
+
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
         event.preventDefault();
@@ -25,6 +33,7 @@ const Airdrop: FC<AirdropProps> = ({pubkey}) => {
             .then((confirmation: RpcResponseAndContext<SignatureResult>) => {
                 console.log(`Confirmation slot: ${confirmation.context.slot}`);
                 setSlot(confirmation.context.slot);
+                connection.getBalance(pubkey).then(setBalance);
             });
         })
         .catch(console.error);
@@ -48,6 +57,8 @@ const Airdrop: FC<AirdropProps> = ({pubkey}) => {
             <hr/>
             { txid ? <p>Transaction: {txid}</p> : null }
             { slot ? <p>Confirmation slot: {slot}</p> : null }
+            <hr/>
+            <p>Your current balance is: {balance}</p>
         </div>
         );
 }
