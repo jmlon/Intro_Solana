5a26b1008d5ca6134a7925f3bbd926078e6f67df
JorgeM LondonoP <jmlon@yahoo.com>
2022-01-14 10:33:04 -0500

    Made the Connection object persistent

--- solana_part2/src/components/Airdrop.tsx
+++ solana_part2/src/components/Airdrop.tsx
@@ -1,5 +1,5 @@
 import { Connection, PublicKey, clusterApiUrl, RpcResponseAndContext, SignatureResult } from "@solana/web3.js";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 
 interface AirdropProps {
     pubkey: PublicKey;
@@ -9,6 +9,10 @@ const network = "devnet";
 
 const Airdrop: FC<AirdropProps> = ({pubkey}) => {
 
+    // Create a connection to blockchain and
+    // make it persistent across renders
+    const connection = useRef(new Connection(clusterApiUrl(network)));
+
     const [ publickey ] = useState<string>(pubkey.toBase58());
     const [ lamports, setLamports ] = useState(100000);
     const [ txid, setTxid ] = useState<string | null>(null);
@@ -17,23 +21,21 @@ const Airdrop: FC<AirdropProps> = ({pubkey}) => {
 
     // Retrieve the balance when mounting the component
     useEffect( () => {
-        const connection = new Connection(clusterApiUrl(network));
-        connection.getBalance(pubkey).then(setBalance);
-    }, []);
+        connection.current.getBalance(pubkey).then(setBalance);
+    }, [pubkey]);
 
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
         event.preventDefault();
-        const connection = new Connection(clusterApiUrl(network));
-        pubkey && connection.requestAirdrop(pubkey, lamports)
+        pubkey && connection.current.requestAirdrop(pubkey, lamports)
         .then((id: string) => { 
             console.log(`Transaction ID ${id}`);
             setTxid(id);
-            connection.confirmTransaction(id)
+            connection.current.confirmTransaction(id)
             .then((confirmation: RpcResponseAndContext<SignatureResult>) => {
                 console.log(`Confirmation slot: ${confirmation.context.slot}`);
                 setSlot(confirmation.context.slot);
-                connection.getBalance(pubkey).then(setBalance);
+                connection.current.getBalance(pubkey).then(setBalance);
             });
         })
         .catch(console.error);
